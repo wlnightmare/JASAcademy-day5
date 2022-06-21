7cb8d8074fa9ae8440e755fd3bd8d1a681389a2c
Farabi Askar <askar.farabi12@gmail.com>
2022-06-21 19:21:17 +0600

    configuring server v3

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 if (process.env.NODE_ENV === "production"){
   app.use(express.static('build'));  
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 }
 
