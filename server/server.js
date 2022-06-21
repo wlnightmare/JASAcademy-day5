dedaf7c9f4f33b912074c89d492eaf28ad923fd4
Farabi Askar <askar.farabi12@gmail.com>
2022-06-21 19:03:44 +0600

    connecting to server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, 'build', 'public');
 
 app.use(express.static(publicPath));
 
